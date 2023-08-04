6de0b91c62bd0a62894db07dc661871a2814a08d
eduardo henrique <eduardohenriqu3@hotmail.com>
2023-08-04 00:11:36 -0300

    off alterações de conta

--- src/middleware.js
+++ src/middleware.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 // import { NextResponse } from "next/server";
 
 // export default function middleware(req) {
@@ -27,33 +26,3 @@
 //     return NextResponse.redirect("http://localhost:3000/search");
 //   }
 // }
-=======
-import { NextResponse } from "next/server";
-
-export default function middleware(req) {
-  let verify = req.cookies.get("next-auth.session-token");
-  let url = req.url;
-
-  if (!verify && url.includes("/teste")) {
-    return NextResponse.redirect("http://localhost:3000/");
-  }
-
-  if (verify && url === "http://localhost:3000/") {
-    return NextResponse.redirect("http://localhost:3000/teste");
-  }
-
-  if (!verify && url.includes("/feed")) {
-    return NextResponse.redirect("http://localhost:3000/");
-  }
-  if (verify && url === "http://localhost:3000/") {
-    return NextResponse.redirect("http://localhost:3000/feed");
-  }
-
-  if (!verify && url.includes("/search")) {
-    return NextResponse.redirect("http://localhost:3000/");
-  }
-  if (verify && url === "http://localhost:3000/") {
-    return NextResponse.redirect("http://localhost:3000/search");
-  }
-}
->>>>>>> 1da5e50c4c4f1f7530277c60f246e11a8ca6adff
